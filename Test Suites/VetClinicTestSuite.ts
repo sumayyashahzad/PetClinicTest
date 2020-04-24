43f564c79547541dd5f6d9883215625de96679a8
zunair.hussain <zunair.hussain@confiz.com>
2020-04-24 12:31:50 +0500

    zunair commits

---
+++ Test Suites/VetClinicTestSuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>VetClinicTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e935c33b-d512-4d3d-83d1-b3ee62f6d18a</testSuiteGuid>
+   <testCaseLink>
+      <guid>784e4917-ba80-4d19-89f5-ef2ce2103caa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PetClinictestRun</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
